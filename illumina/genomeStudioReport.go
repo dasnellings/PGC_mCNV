fe15166fe26727c462c9e3fa270cfe57db524dd1
Daniel <daniel.snellings@duke.edu>
2022-05-13 17:12:32 -0400

    msg

--- illumina/genomeStudioReport.go
+++ illumina/genomeStudioReport.go
@@ -85,7 +85,7 @@ func processGsHeader2(s string) GsReport {
 	var ans GsReport
 	var err error
 	fields := strings.Split(s, "\t")
-	if len(fields) != 9 {
+	if len(fields) != 18 {
 		log.Panicf("ERROR: following lines has unexpected number of columns:\n%s", s)
 	}
 	ans.Marker = fields[0]
@@ -106,7 +106,7 @@ func processGsHeader3(s string) GsReport {
 	var ans GsReport
 	var err error
 	fields := strings.Split(s, "\t")
-	if len(fields) != 19 {
+	if len(fields) != 9 {
 		log.Panicf("ERROR: following lines has unexpected number of columns:\n%s", s)
 	}
 	ans.Marker = fields[0]
