873b7d247fc29a5cc5694260a8ca560b06e06e14
Daniel <daniel.snellings@duke.edu>
2022-05-25 18:45:05 -0400

    swapBafLrr

---
+++ cmd/swapBafLrr/swapBafLrr.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/vertgenlab/gonomics/exception"
+	"github.com/vertgenlab/gonomics/fileio"
+	"github.com/vertgenlab/gonomics/vcf"
+	"log"
+	"os"
+)
+
+func main() {
+	file := os.Args[1]
+	out := os.Args[2]
+	o := fileio.EasyCreate(out)
+	data, header := vcf.GoReadToChan(file)
+	vcf.NewWriteHeader(o, header)
+	for v := range data {
+		if v.Format[1] != "BAF" || v.Format[2] != "LRR" {
+			log.Panicln("baf and lrr out of order or not found")
+		}
+		for _, i := range v.Samples {
+			i.FormatData[1], i.FormatData[2] = i.FormatData[2], i.FormatData[1]
+		}
+		vcf.WriteVcf(o, v)
+	}
+	err := o.Close()
+	exception.PanicOnErr(err)
+}
